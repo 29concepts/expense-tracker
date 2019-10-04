e8e1771ffbed81fa4fc22cfc925170e776e220bb
Aamnah <hello@aamnah.com>
2019-10-04 14:54:12 +0500

    + navigation and screen placeholders

--- App.tsx
+++ App.tsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import styled from 'styled-components'
+import { createAppContainer } from 'react-navigation' // this wraps the navigator
+import { createBottomTabNavigator } from 'react-navigation-tabs'
 
-export default function App() {
-  return (
-    <ViewContainer>
-      <Greeting>Nice!</Greeting>
-    </ViewContainer>
-  )
+import Home from './screens/Home'
+import Details from './screens/Details'
+import Settings from './screens/Settings'
+import { AntDesign } from '@expo/vector-icons'
+
+const App = () => {
+  return <TabNavigator />
 }
 
 const ViewContainer = styled.View`
@@ -20,3 +23,34 @@ const Greeting = styled.Text`
   font-size: 20;
   color: white;
 `
+
+const TabNavigator = createAppContainer(
+  createBottomTabNavigator(
+    {
+      Home: {
+        screen: Home,
+        navigationOptions: {
+          tabBarIcon: () => <AntDesign name="bars" size={24} color="black" />
+        }
+      },
+      Details: {
+        screen: Details,
+        navigationOptions: {
+          tabBarIcon: () => <AntDesign name="piechart" size={24} color="black" />
+        }
+      },
+      Settings: {
+        screen: Settings,
+        navigationOptions: {
+          tabBarIcon: () => <AntDesign name="setting" size={24} color="black" />
+        }
+      }
+    },
+    {
+      // Options
+      defaultNavigationOptions: ({ navigation }) => ({})
+    }
+  )
+)
+
+export default App
---
+++ screens/Home.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import styled from 'styled-components'
+import { Ionicons } from '@expo/vector-icons'
+
+const Home = () => {
+  return (
+    <ViewContainer>
+      <Greeting>Home</Greeting>
+    </ViewContainer>
+  )
+}
+
+const ViewContainer = styled.View`
+  flex: 1;
+  background-color: salmon;
+  align-items: center;
+  justify-content: center;
+`
+const Greeting = styled.Text`
+  font-weight: bold;
+  font-size: 20;
+  color: white;
+`
+
+export default Home
