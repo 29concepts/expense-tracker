97ce22f5a0aa1ac784adfb3004663cbe02b9b2d6
Aamnah <hello@aamnah.com>
2019-10-04 16:13:04 +0500

    + getting a basic FlatList

--- screens/Home.tsx
+++ screens/Home.tsx
@@ -1,25 +1,58 @@
 import React from 'react'
 import styled from 'styled-components'
+import Constants from 'expo-constants'
 import { Ionicons } from '@expo/vector-icons'
+
+const DATA = [
+  {
+    id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
+    title: 'First Item'
+  },
+  {
+    id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
+    title: 'Second Item'
+  },
+  {
+    id: '58694a0f-3da1-471f-bd96-145571e29d72',
+    title: 'Third Item'
+  }
+]
 
 const Home = () => {
   return (
     <ViewContainer>
-      <Greeting>Home</Greeting>
+      <Transactions
+        data={DATA}
+        renderItem={({ item }) => (
+          <Transaction>
+            <TransactionDetails>{item.title}</TransactionDetails>
+          </Transaction>
+        )}
+        keyExtractor={item => item.id}
+      />
     </ViewContainer>
   )
 }
 
-const ViewContainer = styled.View`
+const ViewContainer = styled.SafeAreaView`
   flex: 1;
-  background-color: salmon;
-  align-items: center;
-  justify-content: center;
+  margin-top: ${Constants.statusBarHeight};
+  background-color: #efefef;
+  color: #333;
+  padding: 8px;
 `
-const Greeting = styled.Text`
-  font-weight: bold;
+
+const Transactions = styled.FlatList`
   font-size: 20;
-  color: white;
+  padding: 8px;
 `
+const Transaction = styled.View`
+  background: white;
+  padding: 8px;
+  margin-bottom: 8px;
+  border-radius: 8px;
+  box-shadow: 0 3px 5px rgba(0, 0, 0, 0.1);
+`
+const TransactionDetails = styled.Text``
 
 export default Home
